41bf1c585b116d42528c24f1a6ec2c0ea5a69d69
unknown <medamineouslati85@gmail.com>
2023-08-07 11:34:22 +0200

    firstJSX

---
+++ Week1/Day 3/Practice/first-react/first-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <p>Things I need to do:</p>
+      <ul>
+        <li>Lern react</li>
+        <li>Climb Mt. Everest</li>
+        <li>Run a marathon</li>
+        <li>Feed the dog</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
